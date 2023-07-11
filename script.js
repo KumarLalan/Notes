3b95b95f54cde49e7c0b52140c2dc497125c7d8b
KumarLalan <125983125+KumarLalan@users.noreply.github.com>
2023-07-11 07:13:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const addbtn = document.querySelector(".addnote");
+const main = document.querySelector(".main");
+const savenotes = ()=>{
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes);
+    const data = [];
+    notes.forEach((note)=>{
+        data.push(note.value)
+    })
+    if(data.length===0){
+        localStorage.removeItem("notes")
+    }
+    else{
+localStorage.setItem("notes",JSON.stringify(data));
+    }
+}
+
+addbtn.addEventListener(
+    "click",function () {
+        addnote() }
+)
+
+const addnote = (text="") =>{
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div class="tool">
+    <i class=" save fa-regular fa-floppy-disk"></i>
+    <i class=" trash fa-regular fa-trash-can"></i>
+</div>
+<textarea>${text} </textarea>`
+note.querySelector(".trash").addEventListener("click", function(){
+    note.remove()
+    savenotes()
+})
+note.querySelector(".save").addEventListener("click",function(){
+    savenotes()
+})
+note.querySelector("textarea").addEventListener("focusout",function(){
+    savenotes();
+})
+main.appendChild(note);
+savenotes();
+}
+
+(
+    function(){
+      const lsnotes  = JSON.parse(localStorage.getItem("notes"));
+      if(lsnotes === null){
+       addnote();
+      }
+      else{
+        lsnotes.forEach(
+            (lsnote)=>{
+                addnote(lsnote)
+            }
+          )   
+      }  
+    }
+)()
